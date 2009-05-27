3692fc7eab86f92aeb4868da6aa325ea991127bc
Adrian Likins <alikins@redhat.com>
2009-05-27 16:18:24 -0400

    add certmaster cert dirs to the setup.py so the spec file sees them
    rev version to 0.25

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 #from setuptools import setup,find_packages
 
 NAME = "certmaster"
-VERSION = "0.24" 
+VERSION = "0.25" 
 SHORT_DESC = "%s remote configuration and management api" % NAME
 LONG_DESC = """
 A small pluggable xml-rpc daemon used by %s to implement various web services hooks
@@ -18,6 +18,9 @@ if __name__ == "__main__":
         initpath   = "/etc/init.d/"
         logpath    = "/var/log/%s/" % NAME
 	certdir    = "/var/lib/%s/" % NAME
+	certmaster_cert_dir = "/var/lib/%s/%s" % (NAME,NAME)
+	certmaster_cert_certs_dir = "/var/lib/%s/%s/certs" % (NAME, NAME)
+	certmaster_cert_csrs_dir = "/var/lib/%s/%s/csrs" % (NAME, NAME)
 	trigpath   = "/var/lib/%s/triggers/"% NAME
         pkipath    = "/etc/pki/%s" % NAME
         rotpath    = "/etc/logrotate.d"
@@ -48,6 +51,9 @@ if __name__ == "__main__":
 			      (rotpath,  ['etc/certmaster_rotate']),
                               (logpath,  []),
 			      (certdir,  []),
+			      (certmaster_cert_dir, []),
+			      (certmaster_cert_certs_dir, []),
+			      (certmaster_cert_csrs_dir, []),
 			      (etcpath,  []),
 			      (pkipath,  []),
 			      (aclpath,  []),
